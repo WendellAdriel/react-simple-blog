6bfbb031575fd7077cd6aad8a070c67ff7586b84
Wendell Adriel <wendelladriel.ti@gmail.com>
2017-07-18 21:15:23 +0100

    finished create post component

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,12 +3,23 @@ import axios from 'axios'
 import { ROOT_URL, API_KEY } from '../config'
 
 export const FETCH_POSTS = 'fetch_posts'
+export const CREATE_POST = 'create_post'
 
 export function fetchPosts () {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost (values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback())
+
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
--- src/containers/posts-new.js
+++ src/containers/posts-new.js
@@ -1,27 +1,82 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createPost } from '../actions'
 
 class PostsNew extends Component {
-  renderTitleField (field) {
+  renderField (field) {
+    const { meta: { touched, error, invalid } } = field
+    const divClass = `form-group ${ touched && invalid ? 'has-danger' : ''  }`
+
     return (
-      <div>
-        <input type="text" { ...field.input } />
+      <div className={ divClass }>
+        <label>{ field.label }</label>
+        <input
+          className="form-control"
+          type="text" { ...field.input }
+        />
+        <div className="text-help">
+          <small>{ touched ? error : '' }</small>
+        </div>
       </div>
     )
   }
 
+  onSubmit (values) {
+    this.props.createPost(values, () => {
+      this.props.history.push('/')
+    })
+  }
+
   render () {
+    const { handleSubmit } = this.props
+
     return(
-      <form>
+      <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
         <Field
+          label="Title"
           name="title"
-          component={ this.renderTitleField }
+          component={ this.renderField }
         />
+        <Field
+          label="Categories"
+          name="categories"
+          component={ this.renderField }
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={ this.renderField }
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
 }
 
+function validate (values) {
+  const errors = {}
+
+  if (!values.title) {
+    errors.title = 'Enter a title!'
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter some categories!'
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter some content please!'
+  }
+
+  return errors
+}
+
 export default reduxForm({
+  validate,
   form: 'CreatePost'
-})(PostsNew)
+})(
+  connect(null, { createPost })(PostsNew)
+)
